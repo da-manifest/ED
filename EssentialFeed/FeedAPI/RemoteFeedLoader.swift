eb617a89df52bea71a0d3407850ab995f3123284
Maksim Khozyashev <0758@inbox.ru>
2021-08-31 16:12:14 +0500

    Add RemoteFeedLoader Result type with success/failure cases

--- EssentialFeed/FeedAPI/RemoteFeedLoader.swift
+++ EssentialFeed/FeedAPI/RemoteFeedLoader.swift
@@ -26,6 +26,12 @@ public final class RemoteFeedLoader {
         case invalidData
     }
     
+    public enum Result: Equatable {
+        
+        case success([FeedItem])
+        case failure(Error)
+    }
+    
     private let url: URL
     private let client: HTTPClient
     
@@ -34,13 +40,13 @@ public final class RemoteFeedLoader {
         self.client = client
     }
     
-    public func load(completion: @escaping (Error) -> Void) {
+    public func load(completion: @escaping (Result) -> Void) {
         client.get(from: url) { result in
             switch result {
             case .success:
-                completion(.invalidData)
+                completion(.failure(.invalidData))
             case .failure:
-                completion(.connectivity)
+                completion(.failure(.connectivity))
             }
         }
     }
