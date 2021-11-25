7343b79f7364123b29880d53ce9de75cf3e5a61e
Maksim Khozyashev <0758@inbox.ru>
2021-11-25 16:34:39 +0500

    Move 'FeedCachePolicy' to its own file

---
+++ EssentialFeed/FeedCache/FeedCachePolicy.swift
@@ -0,0 +1,27 @@
+//
+//  FeedCachePolicy.swift
+//  EssentialFeed
+//
+//  Created by Maksim Khozyashev on 25.11.2021.
+//
+
+import Foundation
+
+internal enum FeedCachePolicy {
+    
+    private enum Constants {
+        
+        static let maxCacheAgeInDays = 7
+    }
+    
+    private static let calendar = Calendar(identifier: .gregorian)
+    
+    internal static func validate(_ timestamp: Date, against date: Date) -> Bool {
+        guard let maxCacheAge = calendar.date(byAdding: .day,
+                                              value: Constants.maxCacheAgeInDays,
+                                              to: timestamp) else {
+            return false
+        }
+        return date < maxCacheAge
+    }
+}
