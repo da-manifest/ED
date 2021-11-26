fc7b4cf4bf3b1b29696d382964eb8d006896d8bb
Maksim Khozyashev <0758@inbox.ru>
2021-11-26 15:56:48 +0500

    Extract duplicate store creation into a helper factory method

--- EssentialFeedTests/FeedCache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/FeedCache/CodableFeedStoreTests.swift
@@ -73,23 +73,13 @@ class CodableFeedStoreTests: XCTestCase {
     override func setUp() {
         super.setUp()
         
-        let storeURL = FileManager.default.urls(for: .documentDirectory,
-                                                   in: .userDomainMask)
-            .first!
-            .appendingPathComponent("image-feed.store")
-        
-        try? FileManager.default.removeItem(at: storeURL)
+        try? FileManager.default.removeItem(at: storeURL())
     }
     
     override func tearDown() {
         super.tearDown()
         
-        let storeURL = FileManager.default.urls(for: .documentDirectory,
-                                                   in: .userDomainMask)
-            .first!
-            .appendingPathComponent("image-feed.store")
-        
-        try? FileManager.default.removeItem(at: storeURL)
+        try? FileManager.default.removeItem(at: storeURL())
     }
     
     func test_retrieve_deliversEmptyOnEmptyCache() {
@@ -160,12 +150,15 @@ class CodableFeedStoreTests: XCTestCase {
 private extension CodableFeedStoreTests {
     
     func makeSUT(file: StaticString = #file, line: UInt = #line) -> CodableFeedStore {
-        let storeURL = FileManager.default.urls(for: .documentDirectory,
-                                                   in: .userDomainMask)
-            .first!
-            .appendingPathComponent("image-feed.store")
-        let sut = CodableFeedStore(storeURL: storeURL)
+        let sut = CodableFeedStore(storeURL: storeURL())
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
+    }
+    
+    func storeURL() -> URL {
+        return FileManager.default.urls(for: .documentDirectory,
+                                           in: .userDomainMask)
+            .first!
+            .appendingPathComponent("image-feed.store")
     }
 }
