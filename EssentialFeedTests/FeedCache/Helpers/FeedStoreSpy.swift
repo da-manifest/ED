fe256df19045d84bf276d0dbc53c0e92c9ec9526
Maksim Khozyashev <0758@inbox.ru>
2023-05-17 12:42:00 +0300

    Refactor 'CachedFeed' type from enum to tuple since we can represent the absence of a value with an Optional

--- EssentialFeedTests/FeedCache/Helpers/FeedStoreSpy.swift
+++ EssentialFeedTests/FeedCache/Helpers/FeedStoreSpy.swift
@@ -59,11 +59,11 @@ import EssentialFeed
     }
     
     func completeRetrievalWithEmptyCache(at index: Int = 0) {
-		retrievalCompletions[index](.success(.empty))
+		retrievalCompletions[index](.success(nil))
     }
     
     func completeRetrieval(with feed: [LocalFeedImage], timestamp: Date, at index: Int = 0) {
-		retrievalCompletions[index](.success(.found(feed: feed, timestamp: timestamp)))
+		retrievalCompletions[index](.success(CachedFeed(feed: feed, timestamp: timestamp)))
     }
 }
 
