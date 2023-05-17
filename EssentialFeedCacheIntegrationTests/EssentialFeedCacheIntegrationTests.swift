3884b2011294c261d778590327c7a33076064179
Maksim Khozyashev <0758@inbox.ru>
2023-05-17 11:23:16 +0300

    Extract duplicate save cache operation into a shared helper method

--- EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
+++ EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -33,12 +33,7 @@ final class EssentialFeedCacheIntegrationTests: XCTestCase {
 		let sutToPerformLoad = makeSUT()
 		let feed = uniqueImageFeed().models
 
-		let saveExp = expectation(description: "Wait for save completion")
-		sutToPerformSave.save(feed) { saveError in
-			XCTAssertNil(saveError, "Expected to save the feed successfully")
-			saveExp.fulfill()
-		}
-		wait(for: [saveExp], timeout: 1)
+		save(feed, with: sutToPerformSave)
 
 		expect(sutToPerformLoad, toLoad: feed)
 	}
@@ -50,21 +45,10 @@ final class EssentialFeedCacheIntegrationTests: XCTestCase {
 		let firstFeed = uniqueImageFeed().models
 		let lastFeed = uniqueImageFeed().models
 
-		let saveExp1 = expectation(description: "Wait for save completion")
-		sutToPerformFirstSave.save(firstFeed) { saveError in
-			XCTAssertNil(saveError, "Expected to save the feed successfully")
-			saveExp1.fulfill()
-		}
-		wait(for: [saveExp1], timeout: 1)
+		save(firstFeed, with: sutToPerformFirstSave)
+		save(lastFeed, with: sutToPerformLastSave)
 
-		let saveExp2 = expectation(description: "Wait for save completion")
-		sutToPerformLastSave.save(lastFeed) { saveError in
-			XCTAssertNil(saveError, "Expected to save the feed successfully")
-			saveExp2.fulfill()
-		}
-		wait(for: [saveExp2], timeout: 1)
-
-		expect(sutToPerformLoad, toLoad: lastFeed)
+		expect(sutToPerformLoad, toLoad: lastFeed )
 	}
 
 	// MARK: - Helpers
@@ -88,6 +72,15 @@ final class EssentialFeedCacheIntegrationTests: XCTestCase {
 			case let .failure(error):
 				XCTFail("Expected successful feed result, got \(error) instead", file: file, line: line)
 			}
+			exp.fulfill()
+		}
+		wait(for: [exp], timeout: 1)
+	}
+
+	private func save(_ feed: [FeedImage], with loader: LocalFeedLoader, file: StaticString = #file, line: UInt = #line) {
+		let exp = expectation(description: "Wait for save completion")
+		loader.save(feed) { saveError in
+			XCTAssertNil(saveError, "Expected to save the feed successfully", file: file, line: line)
 			exp.fulfill()
 		}
 		wait(for: [exp], timeout: 1)
