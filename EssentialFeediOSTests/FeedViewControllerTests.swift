395522be09b49e15258ae3b310bea61959322a64
Maksim Khozyashev <0758@inbox.ru>
2023-05-18 15:35:48 +0300

    Cancel image URL preloading when image view is not near visible anymore

--- EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -219,6 +219,22 @@ final class FeedViewControllerTests: XCTestCase {
 		XCTAssertEqual(loader.loadedImageURLs, [image0.url, image1.url], "Expected second image URL request once second image is near visible")
 	}
 
+	func test_feedImageView_cancelsImageURLPreloadinWhenNotNearVisibleAnymore() {
+		let image0 = makeImage(url: URL(string: "http://url-0.com")!)
+		let image1 = makeImage(url: URL(string: "http://url-1.com")!)
+		let (sut, loader) = makeSUT()
+
+		sut.loadViewIfNeeded()
+		loader.completeFeedLoading(with: [image0, image1])
+		XCTAssertEqual(loader.canceledImageURLs, [], "Expected no canceled image URL requests until image is not neear visible")
+
+		sut.simulateFeedImageViewNotNearVisible(at: 0)
+		XCTAssertEqual(loader.loadedImageURLs, [image0.url], "Expected first canceled image URL request once first image is not near visible anymore")
+
+		sut.simulateFeedImageViewNotNearVisible(at: 1)
+		XCTAssertEqual(loader.loadedImageURLs, [image0.url, image1.url], "Expected second canceled image URL request once second image is not near visible anymore")
+	}
+
 	// MARK: - Helpers
 	private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: FeedViewController, loader: LoaderSpy) {
 		let loader = LoaderSpy()
@@ -333,6 +349,14 @@ private extension FeedViewController {
 		let ds = tableView.prefetchDataSource
 		let index = IndexPath(row: row, section: feedImagesSection)
 		ds?.tableView(tableView, prefetchRowsAt: [index])
+	}
+
+	func simulateFeedImageViewNotNearVisible(at row: Int) {
+		simulateFeedImageViewVisible(at: row)
+
+		let ds = tableView.prefetchDataSource
+		let index = IndexPath(row: row, section: feedImagesSection)
+		ds?.tableView?(tableView, cancelPrefetchingForRowsAt: [index])
 	}
 
 	func numberOfRenderedFeedImageViews() -> Int {
