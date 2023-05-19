5bdd5fbba965a9b70e041411f844711849bf06ee
Maksim Khozyashev <0758@inbox.ru>
2023-05-19 11:45:22 +0300

    Extract 'UIRefreshControl' creation/configuration and refresh logic with 'FeedLoader' to the new 'FeedRefreshViewController'

---
+++ EssentialFeediOS/Feed UI/Controllers/FeedRefreshViewController.swift
@@ -0,0 +1,36 @@
+//
+//  FeedRefreshViewController.swift
+//  EssentialFeediOS
+//
+//  Created by Maksim Khozyashev on 19.05.2023.
+//
+
+import UIKit
+import EssentialFeed
+
+public final class FeedRefreshViewController: NSObject {
+	public var onRefresh: (([FeedImage]) -> Void)?
+
+	private(set) lazy var view: UIRefreshControl = {
+		let view = UIRefreshControl()
+		view.addTarget(self, action: #selector(refresh), for: .valueChanged)
+		return view
+	}()
+
+	private let feedLoader: FeedLoader
+
+	init(feedLoader: FeedLoader) {
+		self.feedLoader = feedLoader
+	}
+
+	@objc func refresh() {
+		view.beginRefreshing()
+		feedLoader.load { [weak self] result in
+			if let feed = try? result.get() {
+				self?.onRefresh?(feed)
+			}
+
+			self?.view.endRefreshing()
+		}
+	}
+}
