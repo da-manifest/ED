d739088eafafb1ffdc46ea70c97a30bd5a4088ca
Maksim Khozyashev <0758@inbox.ru>
2023-05-19 11:16:03 +0300

    Extract FeedViewController test helpers to a separate file

--- EssentialFeediOSTests/Controllers/FeedViewControllerTests.swift
+++ EssentialFeediOSTests/Controllers/FeedViewControllerTests.swift
@@ -323,57 +323,6 @@ final class FeedViewControllerTests: XCTestCase {
 	}
 }
 
-private extension FeedViewController {
-	var isShowingLoadingIndicator: Bool {
-		refreshControl?.isRefreshing == true
-	}
-
-	@discardableResult
-	func simulateFeedImageViewVisible(at index: Int) -> FeedImageCell? {
-		feedImageView(at: index) as? FeedImageCell
-	}
-
-	func simulateFeedImageViewNotVisible(at row: Int) {
-		let view = simulateFeedImageViewVisible(at: row)
-
-		let delegate = tableView.delegate
-		let index = IndexPath(row: row, section: feedImagesSection)
-		delegate?.tableView?(tableView, didEndDisplaying: view!, forRowAt: index)
-	}
-
-	func simulateUserInitiatedFeedReload() {
-		refreshControl?.simulatePullToRefresh()
-	}
-
-	func simulateFeedImageViewNearVisible(at row: Int) {
-		let ds = tableView.prefetchDataSource
-		let index = IndexPath(row: row, section: feedImagesSection)
-		ds?.tableView(tableView, prefetchRowsAt: [index])
-	}
-
-	func simulateFeedImageViewNotNearVisible(at row: Int) {
-		simulateFeedImageViewVisible(at: row)
-
-		let ds = tableView.prefetchDataSource
-		let index = IndexPath(row: row, section: feedImagesSection)
-		ds?.tableView?(tableView, cancelPrefetchingForRowsAt: [index])
-	}
-
-	func numberOfRenderedFeedImageViews() -> Int {
-		return tableView.numberOfRows(inSection: feedImagesSection)
-	}
-
-	func feedImageView(at row: Int) -> UITableViewCell? {
-		let ds = tableView.dataSource
-		let index = IndexPath(row: row, section: feedImagesSection)
-		return ds?.tableView(tableView, cellForRowAt: index)
-	}
-
-	private var feedImagesSection: Int {
-		return 0
-	}
-}
-
 private extension FeedImageCell {
 	var isShowingLocation: Bool {
 		!locationContainer.isHidden
