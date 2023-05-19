29b3c55caffc0dbcdbc8004675289693a6a74801
Maksim Khozyashev <0758@inbox.ru>
2023-05-19 11:04:49 +0300

    Extract UIImage test helper to a separate file

--- EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -415,19 +415,6 @@ private extension UIRefreshControl {
 	}
 }
 
-private extension UIImage {
-	static func makeImage(withColor color: UIColor) -> UIImage {
-		let rect = CGRect(x: 0, y: 0, width: 1, height: 1)
-		UIGraphicsBeginImageContext(rect.size)
-		let context = UIGraphicsGetCurrentContext()
-		context?.setFillColor(color.cgColor)
-		context?.fill(rect)
-		let img = UIGraphicsGetImageFromCurrentImageContext()
-		UIGraphicsEndImageContext()
-		return img!
-	}
-}
-
 private extension UIButton {
 	func simulateTap() {
 		allTargets.forEach { target in
