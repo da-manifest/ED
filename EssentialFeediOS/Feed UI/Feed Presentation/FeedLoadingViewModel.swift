a052f720985bf898facd173dd16e7baef6e0f7e3
Maksim Khozyashev <0758@inbox.ru>
2023-05-19 16:20:25 +0300

    Move ViewModels to separate files

---
+++ EssentialFeediOS/Feed UI/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Maksim Khozyashev on 19.05.2023.
+//
+
+struct FeedLoadingViewModel {
+	let isLoading: Bool
+}
---
+++ EssentialFeediOS/Feed UI/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Maksim Khozyashev on 19.05.2023.
+//
+
+import EssentialFeed
+
+struct FeedViewModel {
+	let feed: [FeedImage]
+}
