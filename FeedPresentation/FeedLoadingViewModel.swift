f6dc28951d8f4d6bdd3dbe2a77a85d0bac97c1c1
Maksim Khozyashev <0758@inbox.ru>
2023-07-10 16:47:41 +0300

    Move 'FeedErrorViewModel' , 'FeedLoadingViewModel', 'FeedViewModel' into separate files

---
+++ FeedPresentation/FeedLoadingViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeedTests
+//
+//  Created by Maksim Khozyashev on 10.07.2023.
+//
+
+import Foundation
+
+
+public struct FeedLoadingViewModel {
+	public let isLoading: Bool
+}
---
+++ FeedPresentation/FeedViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeedTests
+//
+//  Created by Maksim Khozyashev on 10.07.2023.
+//
+
+import Foundation
+
+
+public struct FeedViewModel {
+	public let feed: [FeedImage]
+}
