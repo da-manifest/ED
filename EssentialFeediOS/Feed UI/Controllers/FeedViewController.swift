a558d17511881f233a23b84e447aea16285810f4
Maksim Khozyashev <0758@inbox.ru>
2023-07-10 16:54:16 +0300

    Replace 'FeedPresenter' from iOS module with the 'FeedPresenter' from the cross platform EssentialFeed module

--- EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
+++ EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import EssentialFeed
 
 protocol FeedViewControllerDelegate {
 	func didRequestFeedRefresh()
@@ -53,11 +54,11 @@ public final class FeedViewController: UITableViewController, UITableViewDataSou
 		indexPaths.forEach(cancelCellControllerLoad)
 	}
 
-	func display(_ viewModel: FeedLoadingViewModel) {
+	public func display(_ viewModel: FeedLoadingViewModel) {
 		refreshControl?.update(isRefreshing: viewModel.isLoading)
 	}
 
-	func display(_ viewModel: FeedErrorViewModel) {
+	public func display(_ viewModel: FeedErrorViewModel) {
 		errorView?.message = viewModel.message
 	}
 
