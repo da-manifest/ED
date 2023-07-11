fe0e8f4d3c97f840e5e19816b74e9c5aa0989c14
Maksim Khozyashev <0758@inbox.ru>
2023-07-11 14:31:56 +0300

    Extract duplicate base URL creation into a factory helper

--- EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -44,9 +44,8 @@ class EssentialFeedAPIEndToEndTests: XCTestCase {
     
     // MARK: - Helpers
 	private func getFeedResult(file: StaticString = #filePath, line: UInt = #line) -> FeedLoader.Result? {
-        let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
         let client = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
-        let loader = RemoteFeedLoader(url: testServerURL, client: client)
+        let loader = RemoteFeedLoader(url: feedTestServerURL, client: client)
         trackForMemoryLeaks(client, file: file, line: line)
         trackForMemoryLeaks(loader, file: file, line: line)
         
@@ -62,7 +61,7 @@ class EssentialFeedAPIEndToEndTests: XCTestCase {
     }
 
 	private func getFeedImageDataResult(file: StaticString = #file, line: UInt = #line) -> FeedImageDataLoader.Result? {
-		let testServerURL = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")!
+		let url = feedTestServerURL.appendingPathComponent("73A7F70C-75DA-4C2E-B5A3-EED40DC53AA6/image")
 		let client = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
 		let loader = RemoteFeedImageDataLoader(client: client)
 		trackForMemoryLeaks(client, file: file, line: line)
@@ -71,7 +70,7 @@ class EssentialFeedAPIEndToEndTests: XCTestCase {
 		let exp = expectation(description: "Wait for load completion")
 
 		var receivedResult: FeedImageDataLoader.Result?
-		_ = loader.loadImageData(from: testServerURL) { result in
+		_ = loader.loadImageData(from: url) { result in
 			receivedResult = result
 			exp.fulfill()
 		}
@@ -79,7 +78,11 @@ class EssentialFeedAPIEndToEndTests: XCTestCase {
 
 		return receivedResult
 	}
-    
+
+	private var feedTestServerURL: URL {
+		return URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
+	}
+
     private func expectedImage(at index: Int) -> FeedImage {
         return FeedImage(id: id(at: index),
                         description: description(at: index),
